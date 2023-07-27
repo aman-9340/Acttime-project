6f37d8dbb52a883214d5f46df5cb520e38893b27
AMAN KHARE <AMAN KHARE@MSI>
2023-07-27 20:30:20 +0530

    demo

---
+++ Demo/src/Sum10.java
@@ -0,0 +1,12 @@
+
+public class Sum10 {
+	public static void main(String[] arg)
+	{
+		int i=1;
+		int sum=0;
+		for(i=1;i<=10;i++)
+		{sum= sum+ i;
+		System.out.println(sum);
+		}sum++;
+	}
+}
---
+++ Demo/src/demo001.java
@@ -0,0 +1,15 @@
+
+public class demo001 {
+  public static void print(){
+	  System.out.println("ok");
+	  
+	System.out.println(1+2);
+  }
+	public static void main(String[] args) {
+		System.out.println("hii");
+		System.out.println("bye");
+		
+		print();
+	}
+
+}
